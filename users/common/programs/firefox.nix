6a5eab57b2397af488e5155e0f5fa6af70d475e4
Daniel Shiposha <ds@unique.network>
2024-02-03 22:18:43 +0100

    chore: next iter

--- users/common/programs/firefox.nix
+++ users/common/programs/firefox.nix
@@ -1,5 +1,23 @@
+let
+  extensionUrl = name: version: "https://addons.mozilla.org/firefox/downloads/latest/${name}/${version}.xpi";
+in
 {
   programs.firefox = {
     enable = true;
+    policies = {
+      DisableTelemetry = true;
+
+      Preferences = {
+        xpinstall.signatures.required = false;
+      };
+
+      Extensions = {
+        Install = [
+          (extensionUrl "grammarly-1" "latest")
+          (extensionUrl "polkadot-js-extension" "latest")
+          (extensionUrl "ether-metamask" "latest")
+        ];
+      };
+    };
   };
 }
