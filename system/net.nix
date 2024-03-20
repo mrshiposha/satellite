71cd3e50251ec72ea570f22b085b470437717479
Daniel Shiposha <ds@unique.network>
2024-03-20 12:10:41 +0700

    fix: add default nameservers

--- system/net.nix
+++ system/net.nix
@@ -1,10 +1,15 @@
 {
-  networking.hostName = "satellite";
-
-  networking.wireless.iwd.enable = true;
-  networking.networkmanager = {
-    enable = true;
-    wifi.backend = "iwd";
+  networking = {
+    hostName = "satellite";
+    wireless.iwd.enable = true;
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+    };
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
   };
 
   services.automatic-timezoned.enable = true;
