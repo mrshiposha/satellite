f6edbef33f0ff678bfa4fc18a5665cbe8a377bae
Daniel Shiposha <ds@unique.network>
2024-06-21 02:43:31 +0200

    fix: use networkd

--- system/net.nix
+++ system/net.nix
@@ -1,17 +1,15 @@
 {
   networking = {
     hostName = "satellite";
+    useNetworkd = true;
     wireless.iwd.enable = true;
-    networkmanager = {
-      enable = true;
-      wifi.backend = "iwd";
-    };
     nameservers = [
       "1.1.1.1"
       "8.8.8.8"
     ];
   };
 
+  systemd.network.enable = true;
   services.automatic-timezoned.enable = true;
 
   services.openssh.enable = true;
