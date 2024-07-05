0c4aca903b8cfe811b132cf63ebb93dfa69e1be4
Daniel Shiposha <ds@unique.network>
2024-07-06 01:22:06 +0200

    fix: use monospace in terminal

--- common/user/programs/basic/wezterm.nix
+++ common/user/programs/basic/wezterm.nix
@@ -4,6 +4,8 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     extraConfig = ''
+      local wezterm = require "wezterm"
+
       return {
         enable_wayland = false, -- https://github.com/wez/wezterm/issues/4483
 
@@ -11,6 +13,8 @@
         window_background_opacity = 0.7,
         use_fancy_tab_bar = false,
         window_close_confirmation = "NeverPrompt",
+
+	font = wezterm.font "monospace",
 
         colors = {
           tab_bar = {
