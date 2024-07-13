e8545b045168a597c65208f9c31b488af946e106
Daniel Shiposha <ds@unique.network>
2024-07-13 21:14:08 +0200

    fix: zsh delete words

--- common/user/programs/fundamental/zsh/default.nix
+++ common/user/programs/fundamental/zsh/default.nix
@@ -12,7 +12,7 @@
     initExtra = ''
       bindkey "^[[1;5C" forward-word
       bindkey "^[[1;5D" backward-word
-
+      bindkey '^H' backward-kill-word
 
       if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
         source ${./p10k/full.zsh}
