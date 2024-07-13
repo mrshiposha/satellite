b99948ebd28db74781b692bc095296c3d779e14a
Daniel Shiposha <ds@unique.network>
2024-07-13 21:15:48 +0200

    fix: enter terminal mode when in terminal

--- common/user/programs/dev/neovim/lua/terminal.lua
+++ common/user/programs/dev/neovim/lua/terminal.lua
@@ -6,4 +6,14 @@ require("flatten").setup({
         wezterm = true,
     },
 })
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = "startinsert",
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function ()
+	if vim.bo.buftype == "terminal" then
+	    vim.api.nvim_command("startinsert")
+	end
+    end
+})
 
