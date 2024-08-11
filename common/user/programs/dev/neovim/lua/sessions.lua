16abd73767d7c998c7900807e9a17a3d321053cc
Daniel Shiposha <ds@unique.network>
2024-08-12 01:51:57 +0200

    fix: session opts

--- common/user/programs/dev/neovim/lua/sessions.lua
+++ common/user/programs/dev/neovim/lua/sessions.lua
@@ -1,13 +1,10 @@
-local util = require("util")
-
-vim.opt.sessionoptions = "tabpages,curdir,options,globals,localoptions"
+vim.opt.sessionoptions = "buffers,curdir,folds,tabpages,winsize,winpos,options,localoptions"
 
 require("auto-session").setup({
 	log_level = "error",
 	auto_session_suppress_dirs = {"~/", "~/dev", "~/Downloads", "/"},
 	auto_restore_lazy_delay_enabled = false,
-	pre_save_cmds = {util.shutdown_lsp},
-	post_restore_cmds = {"NvimTreeOpen"}
+	post_restore_cmds = {"NvimTreeOpen"},
 })
 
 require("telescope").load_extension("session-lens")
