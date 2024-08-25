8510801006b76b12cc1d9b6d70d2f88807368829
Daniel Shiposha <ds@unique.network>
2024-08-25 17:30:31 +0200

    fix: enter in yazi

--- common/user/modules/programs/yazi.nix
+++ common/user/modules/programs/yazi.nix
@@ -10,6 +10,28 @@ with lib;
 			settings = {
 				show_hidden = false;
 			};
+
+			keymap = {
+				manager.prepend_keymap = [
+					{
+						on = [ "<Enter>" ];
+						run  = "plugin --sync smart-enter";
+						desc = "Enter the child directory, or open the file";
+					}
+				];
+			};
+		};
+
+		xdg.configFile.yazi-smart-enter = {
+			target = "yazi/plugins/smart-enter.yazi/init.lua";
+			text = ''
+				return {
+					entry = function()
+						local h = cx.active.current.hovered
+						ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+						end,
+				}
+			'';
 		};
 	};
 }
