105cba321bdc33821b4eab3905e5400f6da9bd7e
Daniel Shiposha <ds@unique.network>
2024-09-05 02:00:24 +0200

    feat: add fleet, rename system->host

--- flake.nix
+++ flake.nix
@@ -1,22 +1,30 @@
 {
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+		flake-utils.url = "github:numtide/flake-utils";
 		home-manager = {
 			url = "github:nix-community/home-manager/release-24.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		fleet = {
+			url = "github:CertainLach/fleet";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
 
 	outputs =
-		inputs@{ nixpkgs, ... }:
+		inputs@{ nixpkgs, flake-utils, fleet, ... }:
 		let
 			mkLib = nixpkgs: nixpkgs.lib.extend (final: prev: import ./lib);
-
 			lib = mkLib nixpkgs;
-
-			system = import ./lib/system.nix lib inputs;
+			host = import ./lib/host.nix lib inputs;
 		in
-		{
-			nixosConfigurations.satellite = system ./hosts/satellite;
-		};
+			flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = import nixpkgs { inherit system; }; in {
+				devShells.default = pkgs.mkShell {
+					packages = [ fleet.packages.${system}.default ];
+				};
+			}) // {
+				nixosConfigurations.satellite = host ./hosts/satellite;
+			};
 }
